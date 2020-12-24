450bdc85798dd09bf9ca2ae193959b7b60efb51a
mohamd haji <mohamdhaji0007@gmail.com>
2020-12-24 11:35:33 +0200

    edit react-slick css

--- src/pages/Pokemon/index.js
+++ src/pages/Pokemon/index.js
@@ -58,7 +58,6 @@ export default function Pokemon() {
   const [settings, setSettings] = useState(config);
 
   useEffect(() => {
-    console.log("hi")
     const pokemons = [];
     const promises = [];
     for (let i = 1; i <= 18; i++) {
